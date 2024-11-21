8ba04e0d2ae1dcbe4a08982fc1843f9133a14155
brianrompis <brian.rompis@gmail.com>
2024-11-21 10:49:03 +0800

    add button to Google Meet

---
+++ src/components/CallBrian.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="callBrian" class="call-button">
+      Call Brian
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    callBrian() {
+      // Redirect to the Google Meet link
+      window.location.href = "https://meet.google.com/stw-saxz-wja";
+    },
+  },
+};
+</script>
+
+<style>
+.call-button {
+  background-color: #4285f4; /* Google blue */
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.call-button:hover {
+  background-color: #3367d6;
+}
+</style>
